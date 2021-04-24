bc21ee3bfa8286627ab5e684908aa221cf1f6d58
joshivinit2018 <joshivinit2018@gmailcom>
2021-04-24 21:57:03 +0530

    [vinit]: add used ifelse and got Exact output

--- javasnakeladder.java
+++ javasnakeladder.java
@@ -1,63 +1,64 @@
-public class javasnakeladder {
-	
-	
-	static final int NoPlay = 0;
-	static final int Ladder = 1;
-	static final int Snake = 2;
-	static int Sum1 = 0;
-	static int Sum2 = 0;
-	static int c = 0;
-	
-	public static void main(String[] args) {
-	 boolean Player1 = true;
-	
-	
-	while (Sum1 < 100 && Sum2 < 100)	{
-	int current_sum = Player1 ? Sum1 :Sum2 ;	
+import java.util.Random;
+ public class javasnakeladder
+{
+    public static final int startPosition = 0;
+    public static final int winningPosition = 100;
+    public static int position=0;
 
-		int die;
-//using random function
-	die=(int)(Math.random()*6) + 1;
+    public static int throwDie() 
+    {
+	Random random=new Random();
+        int value = random.nextInt(6)+1;
+	return value;
+     }
+     public static int optionForPlay() 
+     {
+	 Random random=new Random();
+         int value1 = random.nextInt(3);
+	 return  value1 ;
+      }
 
-		//System.out.println("start position is zero");
-		System.out.println("Turn: " +( Player1 ? "Player 1" : "Player 2"));
-		System.out.println("The Player rolls the die no is "+die);
-	
-		int opt = (int) Math.floor(Math.random() * 10) % 3;
-
-		if (opt == Ladder) {
-			if (current_sum <= 100 && (current_sum + die <= 100))
-			current_sum = current_sum + die;
-			System.out.println("Ladder");
-}
-		else if (opt == Snake) {
-			
-			if (current_sum > 0 && (current_sum - die) >=0){
-			current_sum = current_sum - die;
-			System.out.println("Snake");
-	}
-}
-else {
-			System.out.println("No play");
-
-}
-			System.out.println("position:" + current_sum);
-			c++;
-			
-			 if (Player1 )
-                        Sum1 = current_sum;
-                else
-                        Sum2 = current_sum;
+      public static void main(String[] args) 
+      {
+         System.out.println("Welcome to Snake and ladder game");
+         System.out.println("Start position is: "  + startPosition);
+         System.out.println("Winning position is: "  + winningPosition);
+       while(position < winningPosition) 
+       {
+          int playValue = optionForPlay();
+          int diceRollValue = throwDie();
+	  System.out.println("~~~ Dice rolled value: "+diceRollValue);
 
 
-			if (opt != Ladder)
-				Player1 = !Player1;
+          if(playValue==0) 
+          {
+             System.out.println("No play");
+             position = position;
+          }
+          else if(playValue==1)
+          {
+             System.out.println("Got Ladder");
+             position = position + diceRollValue;
+             if(position > 100) {
 
-			System.out.println("number of times, the dice was played is "+ c+".");
-			System.out.println(Sum1>=100? "player 1 won" : "player 2 Won");
-}
-
-
-}
-
-}
+		//System.out.println("congratulations player reached to winning position");
+	     }
+          }
+          else if(playValue==2)
+          {
+             System.out.println("Snake Attack");
+             position = position - diceRollValue;
+             if(position < 0) 
+              {
+		position=0;
+               }
+          }
+          System.out.println("new position is : " +position);
+          if(position == 100) 
+          {
+	      System.out.println("congratulations player reached to winning position");
+	      System.out.println("Game ended");
+	  }
+        }
+     }
+ }
